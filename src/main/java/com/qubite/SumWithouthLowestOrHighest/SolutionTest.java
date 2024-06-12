2d7c230e985e16d8dd3b966a68720b2d4e84629f
Ahmed Abd Elrhman <38266577+almaystroOo@users.noreply.github.com>
2024-06-13 01:37:24 +0200

    test case

--- src/main/java/com/qubite/SumWithouthLowestOrHighest/SolutionTest.java
+++ src/main/java/com/qubite/SumWithouthLowestOrHighest/SolutionTest.java
@@ -4,8 +4,11 @@ import static org.junit.Assert.assertEquals;
 import org.junit.runners.JUnit4;
 
 public class SolutionTest {
+    //test code
     @Test
     public void BasicTests() {
         assertEquals(16, Kata.sum(new int[] { 6, 2, 1, 8, 10}));
+        assertEquals(16, Kata.sum(new int[] { 6, 2, 1, 123, 123}));
+
     }
 }
